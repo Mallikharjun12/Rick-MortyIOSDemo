8ae237b20fd05b55a536e1730fb82ca44edb9127
Mallikharjun Kakarla <mallikharjun.kakarla@teamlease.com>
2022-12-26 23:20:42 +0530

    Add Models

---
+++ RickAndMorty/Models/RMOrigin.swift
@@ -0,0 +1,15 @@
+//
+//  RMOrigin.swift
+//  RickAndMorty
+//
+//  Created by Mallikharjun kakarla on 25/12/22.
+//
+
+import Foundation
+
+struct RMOrigin:Codable {
+    
+      let name : String
+      let url : String
+    
+}
---
+++ RickAndMorty/Models/RMSingleLocation.swift
@@ -0,0 +1,15 @@
+//
+//  RMSingleLocation.swift
+//  RickAndMorty
+//
+//  Created by Mallikharjun kakarla on 25/12/22.
+//
+
+import Foundation
+
+struct RMSingleLocation:Codable {
+    
+      let name : String
+      let url : String
+    
+}
